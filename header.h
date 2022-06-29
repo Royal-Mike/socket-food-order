ebd542346c5127c0c7170bba64ad89bd103b24fb
RoyalMike <bibo200311@gmail.com>
2022-06-29 14:51:57 +0700

    initialization

---
+++ header.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_SOCKET
+#define HEADER_SOCKET
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#define DEF_BUF_LEN 512
+#define DEF_PORT "5432"
+
+#endif
